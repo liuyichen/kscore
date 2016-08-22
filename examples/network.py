4d66c24b0fb99828deab84ec85f536f19adcde2d
ziguang <chenziguang@hotmail.com>
2016-08-23 02:55:18 +0800

    fix slb action name

--- examples/network.py
+++ examples/network.py
@@ -14,24 +14,26 @@ if __name__ == "__main__":
     eipClient = s.create_client("eip", "cn-shanghai-2", use_ssl=False)
     slbClient = s.create_client("slb", "cn-shanghai-2", use_ssl=False)
 
-    allVpcs=vpcClient.describe_vpcs()
-    allNics=vpcClient.describe_network_interfaces()
+    #allVpcs=vpcClient.describe_vpcs()
+    #allNics=vpcClient.describe_network_interfaces()
     #allEips=eipClient.describe_addresses(MaxResults=7,NextToken='OA==')
     #allEips=eipClient.describe_addresses(MaxResults=7)
     allEips=eipClient.describe_addresses(**{'Filter.1.Name':'instance-type','Filter.1.Value.1':'Ipfwd'})
     #allEips=eipClient.describe_addresses(Filter.1.Name='instance-type',Filter.1.Value='Slb')
     allListeners=slbClient.describe_listeners(**{'Filter.1.Name':'load-balancer-id','Filter.1.Value.1':'5acac037-531c-414e-bb6f-c639c8948a57'}) 
+    givenListeners=slbClient.describe_listeners(**{'ListenerId.1':'ca2717c8-9bc0-4955-a2f1-2b53b24301b9'}) 
+    slbClient.deregister_instances_from_listener(**{'RegisterId':'b74b0d7f-79f2-4601-980f-326804f22f42'}) 
 
     #pprint.pprint(allEips)
 
     #prettyPrinter().pprint(allVpcs)
-    prettyPrinter().pprint(allEips)
-    prettyPrinter().pprint(allNics)
+    #prettyPrinter().pprint(allEips)
+    #prettyPrinter().pprint(allNics)
     for item in allEips['AddressesSet']:
        print item['PublicIp']
        print item['AllocationId']
-    #eipClient.associate_address(**{'AllocationId':'1cd0da05-8a3e-4c8e-8230-e6d39b85331e','InstanceType':'Ipfwd','InstanceId':'bede9a1c-d3a7-4b31-82e6-6699790ad1a3', 'NetworkInterfaceId':'fec81567-a4c7-4460-a998-54f407e77c0a'})
-    prettyPrinter().pprint(allListeners)
+    #eipClient.associate_address(**{'AllocationId':'1cd0da05-8a3e-4c8e-8230-e6d39b85331e','InstanceType':'Slb','InstanceId':'bede9a1c-d3a7-4b31-82e6-6699790ad1a3', 'NetworkInterfaceId':'fec81567-a4c7-4460-a998-54f407e77c0a'})
     for item in allListeners['ListenerSet']:
        print item['ListenerName']
        print item['ListenerId']
+    prettyPrinter().pprint(givenListeners)
